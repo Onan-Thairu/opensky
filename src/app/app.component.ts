dde5d005bbb4c5e8476de9dd208a028aa5d06416
Onan Thairu <thairuonan@gmail.com>
2024-01-11 17:30:02 +0300

    add the flight data component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FlightDataComponent } from './components/flight-data/flight-data.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FlightDataComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/flight-data/flight-data.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-flight-data',
+  standalone: true,
+  imports: [],
+  templateUrl: './flight-data.component.html',
+  styleUrl: './flight-data.component.css'
+})
+export class FlightDataComponent {
+
+}
