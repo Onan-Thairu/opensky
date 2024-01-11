930e86c10988d1fec70167a4d276801c183f6e5f
Onan Thairu <thairuonan@gmail.com>
2024-01-11 18:09:36 +0300

    chore: add the flight data service.

---
+++ src/app/Flight.ts
@@ -0,0 +1,8 @@
+export interface Flight {
+  icao24: string,
+  callsign: string,
+  origin_country: string,
+  time_position: number,
+  longitude: GLfloat,
+  latitude: GLfloat
+}
--- src/app/services/flight-data.service.ts
+++ src/app/services/flight-data.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { Flight } from '../Flight';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FlightDataService {
-
+  private flights: Flight[] = [
+    {
+      icao24: "icao24",
+      callsign: "callsign",
+      origin_country: "Kenya",
+      time_position: 24524352,
+      longitude: 34.2345243,
+      latitude: -1.123451324
+    },
+    {
+      icao24: "icao24",
+      callsign: "callsign",
+      origin_country: "Somalia",
+      time_position: 24524352,
+      longitude: 34.2345243,
+      latitude: -1.123451324
+    },
+    {
+      icao24: "icao24",
+      callsign: "callsign",
+      origin_country: "South Africa",
+      time_position: 24524352,
+      longitude: 34.2345243,
+      latitude: -1.123451324
+    }
+  ]
   constructor() { }
+
+  getFlightData() {
+    return this.flights
+  }
 }
